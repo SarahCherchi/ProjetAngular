d63944008a456f7977427cac5e9b54970b12dfb8
SarahCherchi <100210990+SarahCherchi@users.noreply.github.com>
2022-12-11 15:27:22 +0100

    création des composants de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { SallesComponent } from './components/salles/salles.component';
+import { CoursComponent } from './components/cours/cours.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    HomeComponent,
+    SallesComponent,
+    CoursComponent
   ],
   imports: [
     BrowserModule,
