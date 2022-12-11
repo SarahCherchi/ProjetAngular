d6d8513fc10353a2aa751d14fc5c748a9157b742
SarahCherchi <100210990+SarahCherchi@users.noreply.github.com>
2022-12-11 15:32:33 +0100

    création des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import {SallesComponent} from './components/salles/salles.component';
+import {CoursComponent} from './components/cours/cours.component';
+import {HomeComponent} from './components/home/home.component';
+const routes: Routes = [
+  {path: 'salles', component: SallesComponent},
+  {path: 'cours', component: CoursComponent},
+  {path: '', component: HomeComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
